14f31b29512f6f19c1031e6129e8c0c6eb926ef4
DJO3 <dev@pytools.org>
2016-01-10 17:46:23 -0500

    updated server.js to include api routes
    added get, post and delete for todos

--- server.js
+++ server.js
@@ -15,6 +15,68 @@ app.use(bodyParser.json())
 app.use(bodyParser.json({type: 'application/vnd.api+json'}))
 app.use(methodOverride())
 
+// define models
+var toDo = mongoose.model('toDo', {
+  text: String
+})
+
+// routes
+
+// api --------------------------------
+
+// get all todos
+app.get('/api/todos', function (req, res) {
+  // use mongoose to get all todos in the database
+  toDo.find(function (err, todos) {
+    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+    if (err) {
+      res.send(err)
+    }
+
+    res.json(todos) // return all todos in JSON format
+  })
+})
+
+// create todo and send back all todos after creation
+app.post('/api/todos', function (req, res) {
+  // create a todo, information comes from AJAX request from Angular
+  toDo.create({
+    text: req.body.text,
+    done: false
+  }, function (err, todo) {
+    if (err) {
+      res.send(err)
+    }
+
+    // get and return all the todos after you create another
+    toDo.find(function (err, todos) {
+      if (err) {
+        res.send(err)
+      }
+      res.json(todos)
+    })
+  })
+})
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function (req, res) {
+  toDo.remove({
+    _id: req.params.todo_id
+  }, function (err, todo) {
+    if (err) {
+      res.send(err)
+    }
+
+    // get and return all the todos after you create another
+    toDo.find(function (err, todos) {
+      if (err) {
+        res.send(err)
+      }
+      res.json(todos)
+    })
+  })
+})
+
 // listen
 app.listen(8080)
 console.log('App listening on port 8080')
